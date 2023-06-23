79eb31ca3ac4f3ac558da9825477c486e0f9f8ec
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-06-23 12:46:09 -0400

    feat: show warning if OPENAI api key is not set (#27)
    * feat: show warning if OPENAI api key is not set
    
    * chore: now more python like
    
    * chore: python fmt

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -6,6 +6,8 @@ import BasicModule from "components/modules/BasicModule";
 import AdvancedModule from "components/modules/AdvancedModule";
 import GitOpsModule from "components/modules/GitOpsModule";
 import Footer from "components/Footer";
+import { useEffect, useState } from "react";
+import Notification from "components/Notification";
 
 const router = createBrowserRouter([
   {
@@ -40,6 +42,30 @@ const router = createBrowserRouter([
 ]);
 
 function Layout() {
+  const [missingOpenAIKey, setMissingAIKey] = useState(false);
+
+  useEffect(() => {
+    // fetch from /config endpoint on backend to get config
+    // and check if openai_api_key is set
+    fetch("http://localhost:9000/config")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `HTTP error! status: ${response.status}; ${response.text}`
+          );
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.openai_api_key === "") {
+          setMissingAIKey(true);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <>
       <Nav />
@@ -49,6 +75,14 @@ function Layout() {
         </main>
         <Footer />
       </div>
+      {missingOpenAIKey && (
+        <Notification type="warning" title="Missing OpenAI API Key">
+          <>
+            <code>OPENAI_API_KEY</code> environment variable not set. Set before
+            continuing for a better experience.
+          </>
+        </Notification>
+      )}
     </>
   );
 }
@@ -56,4 +90,5 @@ function Layout() {
 function App() {
   return <RouterProvider router={router} />;
 }
+
 export default App;
