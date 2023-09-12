2535b92907489741e84ce6652b0f8a0ebb35c0c4
George MacRorie <me@georgemac.com>
2023-09-12 16:08:19 +0100

    feat(cup/argo): add new argo example project

---
+++ cup/argo/cmd/provision/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/exec"
+
+	"sigs.k8s.io/kind/pkg/cluster"
+)
+
+const (
+	clusterName     = "cup-argo"
+	contextName     = "kind-" + clusterName
+	argoInstallYaml = "https://raw.githubusercontent.com/argoproj/argo-cd/master/manifests/install.yaml"
+)
+
+func main() {
+	slog.Info("Checking for cluster...")
+
+	provider := cluster.NewProvider(cluster.ProviderWithDocker())
+	nodes, err := provider.ListNodes(clusterName)
+	exitOnError(err)
+	if len(nodes) < 1 {
+		slog.Info("Creating cluster...")
+
+		err := provider.Create(clusterName, cluster.CreateWithConfigFile("kind-config.yml"))
+		exitOnError(err)
+	}
+
+	slog.Info("Cluster created")
+
+	context, err := kube(nil, "config", "current-context")
+	exitOnError(err)
+
+	if context != contextName {
+		_, err := kube(nil, "config", "set-context", contextName)
+		exitOnError(err)
+	}
+	slog.Info("Context set")
+
+	// create if not exist
+	_, _ = kube(nil, "create", "namespace", "argocd")
+
+	// set namespace on context
+	_, err = kube(nil, "config", "set", "contexts.kind-cup-argo.namespace", "argocd")
+	exitOnError(err)
+	slog.Info("Namespace created")
+
+	slog.Info("Installing Argo...")
+	// get argo install yaml
+	resp, err := http.Get(argoInstallYaml)
+	exitOnError(err)
+
+	if resp.StatusCode != http.StatusOK {
+		exitOnError(fmt.Errorf("unexpected status code %d", resp.StatusCode))
+	}
+
+	// install argocd
+	_, err = kube(resp.Body, "apply", "-f", "-")
+
+	// wait for argo to be ready
+	_, err = kube(nil, "wait", "deployment", "argocd-server", "--for", "condition=Available=true", "--timeout", "60s")
+	exitOnError(err)
+
+	slog.Info("Argo ready")
+
+	// install gitea
+	_, _ = kube(nil, "create", "namespace", "gitea")
+
+	_, err = kube(nil, "apply", "-n", "gitea", "-f", "manifests/gitea.yml")
+	exitOnError(err)
+
+	// wait for gitea provision job to complete
+	_, err = kube(nil, "wait", "-n", "gitea", "jobs", "provision-gitea", "--for", "condition=complete", "--timeout", "60s")
+	exitOnError(err)
+
+	slog.Info("Gitea ready")
+
+	// install cup
+	_, _ = kube(nil, "create", "namespace", "gitea")
+
+	_, err = kube(nil, "apply", "-n", "cup", "-f", "manifests/cup.yml")
+	exitOnError(err)
+
+	// wait for gitea provision job to complete
+	_, err = kube(nil, "wait", "-n", "cup", "deployment", "cup", "--for", "condition=Available=true", "--timeout", "60s")
+	exitOnError(err)
+
+	slog.Info("Cup ready")
+}
+
+func exitOnError(err error) {
+	if err != nil {
+		slog.Error("Exiting...", "error", err)
+		os.Exit(1)
+	}
+}
+
+func kube(stdin io.Reader, args ...string) (string, error) {
+	return sh(stdin, "kubectl", args...)
+}
+
+func sh(stdin io.Reader, cmd string, args ...string) (string, error) {
+	buf := &bytes.Buffer{}
+	command := exec.Command(cmd, args...)
+	command.Stdin = stdin
+	command.Stdout = buf
+	command.Stderr = os.Stderr
+	if err := command.Run(); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
+}
---
+++ cup/argo/go.mod
@@ -0,0 +1,47 @@
+module go.flipt.io/labs/cup/argo
+
+go 1.21.0
+
+require (
+	dagger.io/dagger v0.8.4
+	github.com/docker/docker v24.0.6+incompatible
+	sigs.k8s.io/kind v0.20.0
+)
+
+require (
+	github.com/99designs/gqlgen v0.17.31 // indirect
+	github.com/BurntSushi/toml v1.0.0 // indirect
+	github.com/Khan/genqlient v0.6.0 // indirect
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/adrg/xdg v0.4.0 // indirect
+	github.com/alessio/shellescape v1.4.1 // indirect
+	github.com/docker/distribution v2.8.2+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/safetext v0.0.0-20220905092116-b49f7bc46da2 // indirect
+	github.com/iancoleman/strcase v0.3.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/moby/term v0.5.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/cobra v1.4.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.6 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/tools v0.11.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gotest.tools/v3 v3.5.0 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+)
