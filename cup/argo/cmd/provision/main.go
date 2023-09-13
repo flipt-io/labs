65fdc7cc8b784bcb2543eee296f7a68253667c6e
George MacRorie <me@georgemac.com>
2023-09-13 11:55:55 +0100

    feat(cup/argo): forward argo svc to 8080 by default

--- cup/argo/cmd/provision/main.go
+++ cup/argo/cmd/provision/main.go
@@ -33,72 +33,80 @@ func main() {
 
 	slog.Info("Cluster created")
 
-	context, err := kube(nil, "config", "current-context")
+	context, err := sh("kubectl", args("config", "current-context"))
 	exitOnError(err)
 
 	if context != contextName {
-		_, err := kube(nil, "config", "set-context", contextName)
+		_, err := sh("kubectl", args("config", "set-context", contextName))
 		exitOnError(err)
 	}
-	slog.Info("Context set")
 
-	// create if not exist
-	_, _ = kube(nil, "create", "namespace", "argocd")
+	{
+		slog.Info("Installing Gitea...")
+		// install gitea
+		_, _ = kube("default", args("create", "namespace", "gitea"))
 
-	// set namespace on context
-	_, err = kube(nil, "config", "set", "contexts.kind-cup-argo.namespace", "argocd")
-	exitOnError(err)
-	slog.Info("Namespace created")
+		// configure gitea
+		_, err = kube("gitea", args("apply", "-f", "manifests/gitea.yml"))
+		exitOnError(err)
 
-	slog.Info("Installing Argo...")
-	// get argo install yaml
-	resp, err := http.Get(argoInstallYaml)
-	exitOnError(err)
+		// wait for gitea provision job to complete
+		_, err = kube("gitea", args("wait", "jobs", "provision-gitea", "--for", "condition=complete", "--timeout", "60s"))
+		exitOnError(err)
 
-	if resp.StatusCode != http.StatusOK {
-		exitOnError(fmt.Errorf("unexpected status code %d", resp.StatusCode))
+		slog.Info("Gitea ready", "address", "http://localhost:3000", "username", "cup", "password", "password")
 	}
 
-	// install argocd
-	_, err = kube(resp.Body, "apply", "-f", "-")
+	{
+		slog.Info("Installing Argo...")
+		// create if not exist
+		_, _ = kube("default", args("create", "namespace", "argocd"))
 
-	// wait for argo to be ready
-	_, err = kube(nil, "wait", "deployment", "argocd-server", "--for", "condition=Available=true", "--timeout", "120s")
-	exitOnError(err)
+		// get argo install yaml
+		resp, err := http.Get(argoInstallYaml)
+		exitOnError(err)
 
-	slog.Info("Argo ready")
+		if resp.StatusCode != http.StatusOK {
+			exitOnError(fmt.Errorf("unexpected status code %d", resp.StatusCode))
+		}
 
-	// install gitea
-	_, _ = kube(nil, "create", "namespace", "gitea")
+		// install argocd
+		_, err = kube("argocd", args("apply", "-f", "-"), stdin(resp.Body))
 
-	_, err = kube(nil, "apply", "-n", "gitea", "-f", "manifests/gitea.yml")
-	exitOnError(err)
+		// wait for argo to be ready
+		_, err = kube("argocd", args("wait", "deployment", "argocd-server", "--for", "condition=Available=true", "--timeout", "120s"))
+		exitOnError(err)
 
-	// wait for gitea provision job to complete
-	_, err = kube(nil, "wait", "-n", "gitea", "jobs", "provision-gitea", "--for", "condition=complete", "--timeout", "60s")
-	exitOnError(err)
+		// configure default app via Argo
+		_, err = kube("argocd", args("apply", "-f", "manifests/argo.yml"))
+		exitOnError(err)
 
-	slog.Info("Gitea ready")
+		password, err := kube("argocd", args("get", "-n", "argocd", "secrets", "argocd-initial-admin-secret", "-o=go-template='{{.data.password|base64decode}}'"))
+		exitOnError(err)
+		slog.Info("Argo ready", "address", "http://localhost:8080", "username", "admin", "password", password)
+	}
 
-	// install cup
-	_, _ = kube(nil, "create", "namespace", "cup")
+	{
+		slog.Info("Installing Cup...")
+		// install cup
+		_, _ = kube("default", args("create", "namespace", "cup"))
 
-	_, err = kube(nil, "apply", "-n", "cup", "-f", "manifests/cup.yml")
-	exitOnError(err)
+		_, err = kube("cup", args("apply", "-f", "manifests/cup.yml"))
+		exitOnError(err)
 
-	// wait for gitea provision job to complete
-	_, err = kube(nil, "wait", "-n", "cup", "deployment", "cup", "--for", "condition=Available=true", "--timeout", "60s")
-	exitOnError(err)
+		// wait for gitea provision job to complete
+		_, err = kube("cup", args("wait", "deployment", "cup", "--for", "condition=Available=true", "--timeout", "60s"))
+		exitOnError(err)
 
-	// configure default app via Argo
-	_, err = kube(nil, "apply", "-n", "argocd", "-f", "manifests/argo.yml")
-	exitOnError(err)
+		slog.Info("Cup ready", "address", "http://localhost:8181")
+	}
 
 	// wait for default app to provision
-	_, err = kube(nil, "wait", "-n", "default", "deployment", "my-app", "--for", "condition=Available=true", "--timeout", "120s")
+	_, err = kube("default", args("wait", "deployment", "my-app", "--for", "condition=Available=true", "--timeout", "120s"))
 	exitOnError(err)
 
 	slog.Info("App is ready")
+
 }
 
 func exitOnError(err error) {
@@ -108,16 +116,47 @@ func exitOnError(err error) {
 	}
 }
 
-func kube(stdin io.Reader, args ...string) (string, error) {
-	return sh(stdin, "kubectl", args...)
+func kube(ns string, opts ...option) (string, error) {
+	return sh("kubectl", append([]option{args("-n", ns)}, opts...)...)
 }
 
-func sh(stdin io.Reader, cmd string, args ...string) (string, error) {
+type cmdOptions struct {
+	args   []string
+	stdin  io.Reader
+	stderr io.Writer
+}
+
+type option func(*cmdOptions)
+
+func args(args ...string) option {
+	return func(co *cmdOptions) {
+		co.args = append(co.args, args...)
+	}
+}
+
+func stdin(r io.Reader) option {
+	return func(co *cmdOptions) {
+		co.stdin = r
+	}
+}
+
+func stderr(w io.Writer) option {
+	return func(co *cmdOptions) {
+		co.stderr = w
+	}
+}
+
+func sh(cmd string, opts ...option) (string, error) {
+	var co cmdOptions
+	for _, opt := range opts {
+		opt(&co)
+	}
+
 	buf := &bytes.Buffer{}
-	command := exec.Command(cmd, args...)
-	command.Stdin = stdin
+	command := exec.Command(cmd, co.args...)
+	command.Stdin = co.stdin
+	command.Stderr = co.stderr
 	command.Stdout = buf
-	command.Stderr = os.Stderr
 	if err := command.Run(); err != nil {
 		return "", err
 	}
