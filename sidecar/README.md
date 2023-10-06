8d0ee3906a91df5e0696df47b8b45d8a6934d86f
Yoofi Quansah <ybquansah@gmail.com>
2023-10-06 11:42:09 -0500

    feat: Add local proxy section, and first lab object-store-replication (#50)
    * feat: Add local proxy section, and first lab s3-replication
    
    * chore: add d2 diagram and add various fixes
    
    * chore: reduce svg size
    
    * chore: modify start script
    
    * feat: Add basic go application and modify README
    
    * chore: make changes in regards to bash script and moving things around in README
    
    * chore: rename README and directory
    
    * feat: Add radio button to sample-app to switch between master and sidecar
    
    * feat: Add local FS implementation under different script
    
    * feat: rename top-level directory to sidecar
    
    * chore: change number for last step and delete non-used image
    
    * chore: harcode values for evlauation and seed data into Flipt in script
    
    * chore: modify README to account for recent changes
    
    * feat: add endpoint for users of curl
    
    * chore: Add example to README for curl
    
    * chore: use lastest for all flipt tags and modify diagram
    
    * chore: fix typo for select option

---
+++ sidecar/README.md
@@ -0,0 +1,76 @@
+Flipt as a Sidecar
+------------
+
+The intent of this lab is to explore the ways that clients can achieve fast evaluations from Flipt for their feature flags. The use case here is that users would like data locality for their feature flag state, so they do not have to worry about network latency to evaluate a feature, especially for their most critical application paths.
+
+## Replication
+
+This project is housed under the `replication` directory. The purpose is for a user to run their application which depends on feature flags with a sidecar Flipt process that pulls data from either an Object Store (S3 bucket) or a Local Filesystem to the container. The evaluation data can then be accessed over `localhost` by talking to the Flipt sidecar.
+
+Lets get started!
+
+Prerequisites:
+- [Docker](https://www.docker.com/)
+- [Kubectl](https://kubernetes.io/docs/reference/kubectl/)
+- [Minikube](https://minikube.sigs.k8s.io/docs/)
+
+### Object Store
+
+Run the deploy script `scripts/start-object-store` to provision the cluster and start all the necessary deployments and services.
+
+<img src="./replication/diagrams/diagram-object-store.svg" alt="Object Store Replication" width="500px" />
+
+This above script deploys the following to Kubernetes:
+- `minio`: Object store that has an S3 compatible API
+- `flipt-master`: Serves as the main Flipt application, this is where users will be accessing the UI to make relevant changes
+- `sample-app`: Serves as the pod with Flipt running as a sidecar, `flipt-sidecar`. There is also a container called `evaluation-client` that will make evaluation calls to the sidecar
+- `flipt-exporter`: CronJob that runs on a 1-minute interval that exports data out of the Flipt master, and puts those changes into the object store
+
+### Local FS
+
+Run the deploy script `scripts/start-local` to provision the cluster and start all the necessary deployments and services.
+
+<img src="./replication/diagrams/diagram-local.svg" alt="Local Replication" width="500px" />
+
+The above script deploys all of the components that the Object Store script does, except for `minio`. Instead it uses [Kubernetes PersistentVolumes](https://kubernetes.io/docs/concepts/storage/persistent-volumes/) for the `flipt-exporter` to supply data to.
+
+---
+
+### GUI
+
+Data is seeded into the `flipt-master` instance already via a Kubernetes job called `flipt-seed`. You can now access the `sample-app` and start playing around with the evaluations.
+
+Here are the steps to do so:
+
+1. Access the frontend for the `sample-app` via Kubernetes port-forward
+
+```bash
+$ kubectl port-forward svc/sample-app --namespace default 8000:8000
+```
+
+2. Switch between `Sidecar` and `Master` and choose a flag `flag_001 - flag_010` and evaluate the time difference
+
+
+You can also access the `flipt-master` via Kubernetes port-forward and make changes to the state via the UI:
+
+```bash
+$ kubectl port-forward svc/flipt-master --namespace default 8080:8080
+```
+
+### cURL
+
+For users of the terminal you can also hit an endpoint to see the differences of evaluation without having to use the UI.
+
+1. Port forward `sample-app`
+
+```bash
+$ kubectl port-forward svc/sample-app --namespace default 8000:8000
+```
+
+2. Make request with `curl`
+
+```bash
+$ curl localhost:8000/cli/backend/{backend}/evaluation/{flagKey}
+```
+
+The backend should be of either type (`sidecar` or `master`), and the `flagKey` should be between `flag_001 - flag_010`.
---
+++ sidecar/replication/go/main.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"html/template"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"go.flipt.io/flipt/rpc/flipt/evaluation"
+	sdk "go.flipt.io/flipt/sdk/go"
+	sdkgrpc "go.flipt.io/flipt/sdk/go/grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+)
+
+//go:embed index.tmpl
+var indexTmplContent string
+
+var t = template.Must(template.New("").Parse(indexTmplContent))
+
+type Evaluation struct {
+	Value string
+}
+
+type TemplateData struct {
+	Evaluation []*Evaluation
+}
+
+func getClientFromAddr(addr string) (sdk.SDK, error) {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return sdk.SDK{}, err
+	}
+
+	transport := sdkgrpc.NewTransport(conn)
+
+	return sdk.New(transport), nil
+}
+
+func invokeEvaluation(ctx context.Context, evaluationClient *sdk.Evaluation, flagName, contextKey, contextValue string) (*evaluation.VariantEvaluationResponse, time.Duration, error) {
+	now := time.Now()
+	e, err := evaluationClient.Variant(ctx, &evaluation.EvaluationRequest{
+		NamespaceKey: "default",
+		FlagKey:      flagName,
+		Context: map[string]string{
+			contextKey: contextValue,
+		},
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+	difference := time.Since(now)
+
+	return e, difference, nil
+}
+
+func run() error {
+	evaluations := make([]*Evaluation, 0)
+
+	masterAddr := os.Getenv("FLIPT_MASTER_ADDR")
+	if masterAddr == "" {
+		masterAddr = "flipt-master:9000"
+	}
+
+	sidecarClient, err := getClientFromAddr("localhost:9000")
+	if err != nil {
+		return err
+	}
+
+	masterClient, err := getClientFromAddr(masterAddr)
+	if err != nil {
+		return err
+	}
+	slog.Info("master address", "master-addr", masterAddr)
+
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		tmplData := TemplateData{
+			Evaluation: evaluations,
+		}
+
+		if err := t.Execute(w, tmplData); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	r.Post("/evaluation", func(w http.ResponseWriter, r *http.Request) {
+		flagKey := r.FormValue("flagKey")
+		contextKey := r.FormValue("contextKey")
+		contextValue := r.FormValue("contextValue")
+		backend := r.FormValue("backend")
+
+		var client = sidecarClient
+		if backend == "master" {
+			client = masterClient
+		}
+
+		variantResponse, difference, err := invokeEvaluation(r.Context(), client.Evaluation(), flagKey, contextKey, contextValue)
+		if err != nil {
+			if e, ok := status.FromError(err); ok {
+				switch e.Code() {
+				case codes.NotFound:
+					evaluations = append(evaluations, &Evaluation{
+						Value: fmt.Sprintf("The flag: %s is not found on server took %s to complete evaluation from the backend: %s", flagKey, difference, backend),
+					})
+					http.Redirect(w, r, r.Referer(), http.StatusFound)
+				default:
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+				}
+			}
+			return
+		}
+
+		variantKey := variantResponse.VariantKey
+		if variantKey == "" {
+			variantKey = "None"
+		}
+		evaluation := &Evaluation{
+			Value: fmt.Sprintf("The %s value from evaluation is: %s and took %s to complete evaluation from the backend: %s", flagKey, variantKey, difference, backend),
+		}
+
+		evaluations = append(evaluations, evaluation)
+
+		http.Redirect(w, r, r.Referer(), http.StatusFound)
+	})
+
+	r.Get("/cli/backend/{backend}/evaluation/{flagKey}", func(w http.ResponseWriter, r *http.Request) {
+		flagKey := chi.URLParam(r, "flagKey")
+		backend := chi.URLParam(r, "backend")
+
+		if backend != "sidecar" && backend != "master" {
+			http.Error(w, "please enter either sidecar or master for backend", http.StatusNotFound)
+			return
+		}
+
+		var client = sidecarClient
+		if backend == "master" {
+			client = masterClient
+		}
+
+		variantResponse, difference, err := invokeEvaluation(r.Context(), client.Evaluation(), flagKey, "in_segment", "segment_001")
+		if err != nil {
+			if e, ok := status.FromError(err); ok {
+				switch e.Code() {
+				case codes.NotFound:
+					w.Write([]byte(fmt.Sprintf("The flag: %s is not found on server took %s to complete evaluation from the backend: %s", flagKey, difference, backend)))
+					return
+				default:
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+				}
+			}
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.Write([]byte(fmt.Sprintf("The %s value from evaluation is: %s and took %s to complete evaluation from the backend: %s", flagKey, variantResponse.VariantKey, difference, backend)))
+	})
+
+	server := &http.Server{
+		Addr:    ":8000",
+		Handler: r,
+	}
+
+	slog.Info("staring http server on :8000")
+	return server.ListenAndServe()
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
